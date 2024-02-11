ee780170ff2cb364c73a8616d7170d8fc7f4dcf9
kallolpandit <kallolpandit466@gmail.com>
2024-02-11 19:32:56 +0600

    md device responsive

--- js/script.js
+++ js/script.js
@@ -2,11 +2,3 @@ $(".counter").counterUp({
   delay: 10,
   time: 1000,
 });
-
-// $(".feedback_slide").slick({
-//   slidesToShow: 3,
-//   slidesToScroll: 1,
-//   autoplay: true,
-//   autoplaySpeed: 1500,
-//   arrows: false,
-// });
