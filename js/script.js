56b497f325372b1a1be385aa178fe37a90fbe5dd
kallolpandit <kallolpandit466@gmail.com>
2024-02-11 14:19:51 +0600

    feedback

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(".counter").counterUp({
   time: 1000,
 });
 
-$(".feedback_slide").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1500,
-  arrows: false,
-});
+// $(".feedback_slide").slick({
+//   slidesToShow: 3,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 1500,
+//   arrows: false,
+// });
